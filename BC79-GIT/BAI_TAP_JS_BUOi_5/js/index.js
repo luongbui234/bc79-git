ace311620bc2fa3786460cea30e33185e916eead
luongbui234 <luongbui0610@gamil.com>
2024-12-08 21:17:48 +0700

    da lam dep xong index.html

---
+++ BC79-GIT/BAI_TAP_JS_BUOi_5/js/index.js
@@ -0,0 +1,19 @@
+// start link iframe
+
+document.getElementById("b1").onclick = function () {
+  document.querySelector("iframe").src = "b1.html";
+};
+
+document.getElementById("b2").onclick = function () {
+  document.querySelector("iframe").src = "b2.html";
+};
+
+document.getElementById("b3").onclick = function () {
+  document.querySelector("iframe").src = "b3.html";
+};
+
+document.getElementById("b4").onclick = function () {
+  document.querySelector("iframe").src = "b4.html";
+};
+
+// end link iframe
